9975e6962898650a2cda96fe9388ab5a5a77f19b
Alexandr <sanchous.vas@gmail.com>
2023-07-31 14:43:14 +0300

    refactoring

--- internal/client/http/metrics.go
+++ internal/client/http/metrics.go
@@ -27,6 +27,75 @@ func (user *User) sendUserMetric(url string, request UserMetricStorage) (err err
 	return
 }
 
+func (user *User) streamingMetricsByFloat64(timestamp int64, mStorage metrics.MetricStorage) (err error) {
+	for k, v := range mStorage.Float64Data {
+		request := UserMetricStorage{
+			UUID: user.UUID,
+			MetricStorage: metrics.MetricStorage{
+				Timestamp: timestamp,
+				Float64Data: map[string]float64{
+					k: v,
+				},
+				Uint32Data: map[string]uint32{},
+				Uint64Data: map[string]uint64{},
+			},
+		}
+
+		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
+		if err != nil {
+			return err
+		}
+		log.Printf("Sending metric '%s'...", k)
+	}
+	return
+}
+
+func (user *User) streamingMetricsByUint32(timestamp int64, mStorage metrics.MetricStorage) (err error) {
+	for metricKey, metricVal := range mStorage.Uint32Data {
+		request := UserMetricStorage{
+			UUID: user.UUID,
+			MetricStorage: metrics.MetricStorage{
+				Timestamp:   timestamp,
+				Float64Data: map[string]float64{},
+				Uint32Data: map[string]uint32{
+					metricKey: metricVal,
+				},
+				Uint64Data: map[string]uint64{},
+			},
+		}
+
+		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
+		if err != nil {
+			return err
+		}
+		log.Printf("Sending metric '%s'...", metricKey)
+	}
+	return
+}
+
+func (user *User) streamingMetricsByUint64(timestamp int64, mStorage metrics.MetricStorage) (err error) {
+	for metricKey, metricVal := range mStorage.Uint64Data {
+		request := UserMetricStorage{
+			UUID: user.UUID,
+			MetricStorage: metrics.MetricStorage{
+				Timestamp:   timestamp,
+				Float64Data: map[string]float64{},
+				Uint32Data:  map[string]uint32{},
+				Uint64Data: map[string]uint64{
+					metricKey: metricVal,
+				},
+			},
+		}
+
+		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
+		if err != nil {
+			return err
+		}
+		log.Printf("Sending metric '%s'...", metricKey)
+	}
+	return
+}
+
 func (user *User) SendMetricStorage() (err error) {
 	mStorage, err := metrics.Get()
 	if err != nil {
@@ -51,64 +120,10 @@ func (user *User) StreamingMetrics() (err error) {
 	if err != nil {
 		return
 	}
-
 	timestamp := mStorage.Timestamp
-	for metricKey, metricVal := range mStorage.Float64Data {
-		request := UserMetricStorage{
-			UUID: user.UUID,
-			MetricStorage: metrics.MetricStorage{
-				Timestamp: timestamp,
-				Float64Data: map[string]float64{
-					metricKey: metricVal,
-				},
-				Uint32Data: map[string]uint32{},
-				Uint64Data: map[string]uint64{},
-			},
-		}
-		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
-		if err != nil {
-			return err
-		}
-		log.Printf("Sending metric '%s'...", metricKey)
-	}
 
-	for metricKey, metricVal := range mStorage.Uint32Data {
-		request := UserMetricStorage{
-			UUID: user.UUID,
-			MetricStorage: metrics.MetricStorage{
-				Timestamp:   timestamp,
-				Float64Data: map[string]float64{},
-				Uint32Data: map[string]uint32{
-					metricKey: metricVal,
-				},
-				Uint64Data: map[string]uint64{},
-			},
-		}
-		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
-		if err != nil {
-			return err
-		}
-		log.Printf("Sending metric '%s'...", metricKey)
-	}
-
-	for metricKey, metricVal := range mStorage.Uint64Data {
-		request := UserMetricStorage{
-			UUID: user.UUID,
-			MetricStorage: metrics.MetricStorage{
-				Timestamp:   timestamp,
-				Float64Data: map[string]float64{},
-				Uint32Data:  map[string]uint32{},
-				Uint64Data: map[string]uint64{
-					metricKey: metricVal,
-				},
-			},
-		}
-		err := user.sendUserMetric("http://"+user.Config.ServerAddr, request)
-		if err != nil {
-			return err
-		}
-		log.Printf("Sending metric '%s'...", metricKey)
-	}
-
+	user.streamingMetricsByFloat64(timestamp, mStorage)
+	user.streamingMetricsByUint32(timestamp, mStorage)
+	user.streamingMetricsByUint64(timestamp, mStorage)
 	return
 }
